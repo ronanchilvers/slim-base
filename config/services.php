2adb108fcfb236784b8ce87b36d1cbe379cbc93d
Ronan Chilvers <ronan@d3r.com>
2018-03-19 21:18:37 +0000

    Move services to provider

--- config/services.php
+++ config/services.php
@@ -4,46 +4,4 @@
 //   - $container
 //   - $app
 
-use App\Controller\IndexController;
-use Aura\Sql\ExtendedPdo;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
-use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
-
-// Logger
-$container->set(LoggerInterface::class, function (ContainerInterface $c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Logger('default');
-    $logger->pushHandler(
-        new StreamHandler(
-            $settings['filename']
-        )
-    );
-    Monolog\Registry::addLogger($logger);
-
-    return $logger;
-});
-
-// Twig
-$container->set(Twig::class, function (ContainerInterface $c) {
-    $settings = $c->get('settings')['twig'];
-    $view = new Twig(
-        $settings['templates'],
-        [
-            'cache' => $settings['cache']
-        ]
-    );
-    $request = $c->get('request');
-    $basePath = rtrim(str_ireplace('index.php', '', $request->getUri()->getBasePath()), '/');
-    $view->addExtension(
-        new TwigExtension(
-            $c->get('router'),
-            $basePath
-        )
-    );
-
-    return $view;
-});
+$container->register(new \App\Provider());
---
+++ src/Provider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Registry;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Ronanchilvers\Container\Container;
+use Ronanchilvers\Container\ServiceProviderInterface;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+
+/**
+ * App service provider
+ *
+ * @author Ronan Chilvers <ronan@d3r.com>
+ */
+class Provider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function register(Container $container)
+    {
+        // Logger
+        $container->set(LoggerInterface::class, function (ContainerInterface $c) {
+            $settings = $c->get('settings')['logger'];
+            $logger = new Logger('default');
+            $logger->pushHandler(
+                new StreamHandler(
+                    $settings['filename']
+                )
+            );
+            Registry::addLogger($logger);
+
+            return $logger;
+        });
+
+        // Twig
+        $container->set(Twig::class, function (ContainerInterface $c) {
+            $settings = $c->get('settings')['twig'];
+            $view = new Twig(
+                $settings['templates'],
+                [
+                    'cache' => $settings['cache']
+                ]
+            );
+            $request = $c->get('request');
+            $basePath = rtrim(str_ireplace('index.php', '', $request->getUri()->getBasePath()), '/');
+            $view->addExtension(
+                new TwigExtension(
+                    $c->get('router'),
+                    $basePath
+                )
+            );
+
+            return $view;
+        });
+    }
+}
