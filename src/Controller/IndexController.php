f755fb9891f217dd18db4b349ce5bd19f3298ee3
Ronan Chilvers <ronan@thelittledot.com>
2019-03-07 11:59:26 +0000

    Add View facade using the Slim\Views\Twig service

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Facades\View;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Ronanchilvers\Foundation\Slim\Traits\TwigAwareTrait;
-use Slim\Views\Twig;
 
 /**
  * Controller for the index
@@ -14,19 +14,6 @@ use Slim\Views\Twig;
  */
 class IndexController
 {
-    use TwigAwareTrait;
-
-    /**
-     * Class constructor
-     *
-     * @author Ronan Chilvers <ronan@d3r.com>
-     */
-    public function __construct(
-        Twig $twig
-    ) {
-        $this->setTwig($twig);
-    }
-
     /**
      * Index action
      *
@@ -36,7 +23,7 @@ class IndexController
         ServerRequestInterface $request,
         ResponseInterface $response
     ) {
-        return $this->render(
+        return View::render(
             $response,
             'index/index.html.twig'
         );
